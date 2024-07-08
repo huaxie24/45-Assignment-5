0eab98f3b2129b75d317e7fd63ae0a9922846974
Laila Ali Laghari <162666511+huaxie24@users.noreply.github.com>
2024-07-09 04:48:25 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+var famousPerson = "Albert Einstein";
+var quote = "A person who never made a mistake never tried anything new.";
+console.log("".concat(famousPerson, " once said \"").concat(quote, "\""));
